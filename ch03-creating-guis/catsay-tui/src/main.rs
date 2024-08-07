13cd0b88058e8a3761c24de55951312f7d362736
mindeleven <mindeleven@posteo.de>
2024-08-07 10:02:10 +0200

    Creating a TextView to hold the ASCII-art, adding the TextView as a layer to the main siv program

--- ch03-creating-guis/catsay-tui/src/main.rs
+++ ch03-creating-guis/catsay-tui/src/main.rs
@@ -6,9 +6,26 @@
 /// a very thin wrapper around the ncurses TUI lib
 /// NOTE: The ncurses lib is terribly unsafe
 
+use cursive::views::TextView;
+
 fn main() {
     // creating a Cursive root object
+    // Cursive uses layers to create a stacked view of the components
     let mut siv = cursive::default();
+
+    // now back to our meowing cat
+    // creating a TextView to hold the ASCII-art
+    let cat_text = "Meow!
+ \\
+  \\
+     /\\_/\\
+    ( o o )
+    =( I )=
+    ";  
+    
+    // declaring the app layout
+    // adding the TextView as a layer to the main siv program
+    siv.add_layer(TextView::new(cat_text));
 
     siv.run(); // starting the event loop
 
