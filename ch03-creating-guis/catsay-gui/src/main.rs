0c7f6b37dd0e6e216b9215e4862b057ce68b88a4
mindeleven <mindeleven@posteo.de>
2024-08-07 15:25:33 +0200

    Putting a gtk image into the gtk window

--- ch03-creating-guis/catsay-gui/src/main.rs
+++ ch03-creating-guis/catsay-gui/src/main.rs
@@ -1,5 +1,12 @@
 use gtk::prelude::*;
-use gtk::{ Application, ApplicationWindow };
+use gtk::{ 
+    Application,
+    ApplicationWindow,
+    Box as GtkBox,
+    Image,
+    Label,
+    Orientation
+};
 
 fn main() {
     
@@ -10,6 +17,27 @@ fn main() {
     
     app.connect_activate(|app| {
         let window = ApplicationWindow::new(app);
+
+        // wrapper box to properly control the layout
+        // gtk::Box is a container to display elements in row or column
+        // here vertical orientation for row
+        let layout_box = GtkBox::new(Orientation::Vertical, 0);
+
+        // creating text in a label
+        let label = Label::new(
+            Some("Meow!\n     \\\n      \\")
+        );
+        layout_box.add(&label); // adding element to layout box
+
+        // creating a gtk image widget
+        let cat_image = Image::from_file(
+            "./assets/cat-2669554_640.png"
+        );
+        layout_box.add(&cat_image); // adding element to layout box
+        
+        // adding layout box to window
+        window.add(&layout_box);
+        
         window.set_title("Catsay");
         window.set_default_size(350, 70);
 
