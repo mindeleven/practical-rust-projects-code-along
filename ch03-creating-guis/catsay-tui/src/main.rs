091c73999ad5f24e468a896ca05263bba22fee81
mindeleven <mindeleven@posteo.de>
2024-08-07 09:53:06 +0200

    Added some explanatory comments

--- ch03-creating-guis/catsay-tui/src/main.rs
+++ ch03-creating-guis/catsay-tui/src/main.rs
@@ -1,7 +1,15 @@
-fn main() {
+/// ncurses (new curses) library overview:
+/// https://invisible-island.net/ncurses/announce.html
+/// 
+/// ncurses 5.101.0 on docs.rs:
+/// https://docs.rs/crate/ncurses/5.101.0
+/// a very thin wrapper around the ncurses TUI lib
+/// NOTE: The ncurses lib is terribly unsafe
 
+fn main() {
+    // creating a Cursive root object
     let mut siv = cursive::default();
 
     siv.run(); // starting the event loop
-    
+
 }
