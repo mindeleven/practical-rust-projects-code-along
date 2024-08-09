4bf73db5e36eef9ce976f8d366739b07c035b479
mindeleven <mindeleven@posteo.de>
2024-08-09 09:25:07 +0200

    Abandoned gui-glade project for now because of issues with gtk on m1 mac

--- ch03-creating-guis/catsay-gui-glade/src/main.rs
+++
@@ -1,6 +0,0 @@
-// layout.glade copied from https://github.com/Apress/practical-rust-projects/blob/master/gui/catsay-gui-glade/src/layout.glade
-// that's the book's repo from 2020 because there seems to be no glade file for the revised edition
-
-fn main() {
-    println!("Hello, world!");
-}
